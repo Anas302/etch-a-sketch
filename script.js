2d6a5813e85e2f94bb1bd6b3ce5a7a577299898a
anas302 <anas302.kh@gmail.com>
2022-06-15 11:02:07 +0400

    Modularize code and added button

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 
-const widthPerSquare = Math.floor(window.innerWidth/16) - 4.3;
-let container = document.querySelector("#container");
+createGrid(16);
+activateButton();
 
-for(let j=0; j<16; j++){
-    for(let i=0; i<16; i++){
-        const square = document.createElement("div");
-        square.setAttribute("style", `width:${widthPerSquare}px; height:${widthPerSquare}px;`);
-        square.setAttribute("class", "sqr");
-        container.appendChild(square);
+//when hover any of the squares, change background color to dark brown.
+
+function activateButton(){
+    let button = document.querySelector("button");
+    button.addEventListener("click", (e) => {
+        let userInput;
+        do{
+            userInput = parseInt(prompt("Enter the number of squares per row: "));
+        }while(userInput > 100);
+
+        removeGrid();
+        createGrid(userInput);
+    });
+};
+
+function createSquare(dimension){
+    const square = document.createElement("div");
+    square.setAttribute("style", `width:${dimension}px; height:${dimension}px;`);
+    square.setAttribute("class", "sqr");
+    square.addEventListener("mouseover", (e) => {
+        square.style.backgroundColor = "brown";
+    });
+    return square;
+};
+
+function createGrid(numOfSquares){
+    let container = document.querySelector("#container");
+    const widthPerSquare = Math.ceil(container.offsetWidth/numOfSquares) - 1.3;
+
+    for(let j=0; j<numOfSquares; j++){
+        for(let i=0; i<numOfSquares; i++){
+            const square = createSquare(widthPerSquare);
+            container.appendChild(square);
+        }
     }
-}
+};
 
-const allSquares = document.querySelectorAll(".sqr");
+function removeGrid(){
+    let container = document.querySelector("#container");
+    let allSquares = document.body.getElementsByClassName("sqr");
+    const numOfSquares = allSquares.length;
 
-// for(let i=0; i<16*16; i++){
-//     allSquares[i].addEventListener("mouseover", (e) => {
-//         allSquares[i].style.backgroundColor = "brown";
-//     });
-// }
+    for(let i=numOfSquares - 1; i>=0; i--)
+        container.removeChild(allSquares[i]);
+};
